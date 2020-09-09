025a6fdc516db62cea7317db9f5a07fb25a4918c
neomort <kyle@kyleadams.com>
2020-09-09 10:12:03 -0600

    vowels

---
+++ vowels.js
@@ -0,0 +1,23 @@
+// Create a function that will receive a string of text of any length and return the number of vowels found within the text.
+
+// Restrictions:
+// No helper methods
+
+// Bonus:
+// Return the vowels used and the number of times it was used. 
+
+let string = "baann";
+let num = 0;
+
+console.log(string);
+
+if (string.includes('a','e','i','o','u')){
+console.log("has vowels");
+// num = string.
+// for every vowel in the string, add onw to num and then print
+console.log(num);
+
+}
+else{
+  console.log("no vowels");
+}
